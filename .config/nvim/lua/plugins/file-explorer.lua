26c800069a2033e3016f432c1f01e9acd7299437
stephen.hu <stephen.hu@enflame-tech.com>
2025-10-13 15:58:35 +0800

    配置Neotree文件浏览器Enter键自动关闭
    - 自定义Enter键行为：打开文件后自动关闭侧边栏
    - 对文件夹使用Enter键则展开/折叠
    - 使用vim.schedule异步关闭，避免buffer冲突
    - 添加一些额外的UI配置选项

--- .config/nvim/lua/plugins/file-explorer.lua
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -35,8 +35,25 @@ return {
           width = 30, -- Corresponds to NERDTreeWinSize = 30
           mappings = {
             ["<Esc>"] = "close_window",
+            ["<CR>"] = function(state)
+              -- 自定义Enter行为：打开文件并关闭neo-tree
+              local node = state.tree:get_node()
+              if node.type == "file" then
+                require("neo-tree.sources.filesystem.commands").open(state)
+                -- 使用vim.schedule异步关闭，避免buffer冲突
+                vim.schedule(function()
+                  require("neo-tree.command").execute({ action = "close" })
+                end)
+              else
+                require("neo-tree.sources.filesystem.commands").toggle_node(state)
+              end
+            end,
           },
         },
+        close_if_last_window = false,
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
       })
 
       -- Set the keymap to toggle it, just like in your .vimrc
